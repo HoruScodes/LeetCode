b04ecbdba586729e9559c15394d12d11222b60cb
raahulll <pipaliya1997@gmail.com>
2019-12-16 16:16:33 +0530

    chage range to get correct answer

--- Pascal's Triangle II.py
+++ Pascal's Triangle II.py
@@ -4,7 +4,7 @@ class Solution:
         if k < 0:
             return arr
         arr.append(1)
-        for i in range(1,k):
+        for i in range(0,k):
             for j in range(len(arr)-1,0,-1):
                 arr[j] = arr[j-1] + arr[j]
             arr.append(1)
